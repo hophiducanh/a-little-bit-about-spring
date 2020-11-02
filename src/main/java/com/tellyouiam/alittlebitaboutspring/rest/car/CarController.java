a3647405699bcd45417bb313b50d1d99ed5371eb
Logbasex <contact.hoducanh@gmail.com>
2020-11-02 22:31:53 +0700

    learning Swagger

--- src/main/java/com/tellyouiam/alittlebitaboutspring/rest/car/CarController.java
+++ src/main/java/com/tellyouiam/alittlebitaboutspring/rest/car/CarController.java
@@ -3,11 +3,15 @@ package com.tellyouiam.alittlebitaboutspring.rest.car;
 import com.tellyouiam.alittlebitaboutspring.dto.car.CarRequest;
 import com.tellyouiam.alittlebitaboutspring.entity.car.Car;
 import com.tellyouiam.alittlebitaboutspring.service.car.CarService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import java.util.List;
 
 @RestController
@@ -41,7 +45,8 @@ public class CarController {
 	}
 	
 	@GetMapping(path = "/carsafteryear")
-	public List<Car> findCarsAfterYear(@RequestParam("year") Integer year) {
+	@ApiOperation("Returns list of cars after year[s].")
+	public List<Car> findCarsAfterYear(@ApiParam("The number of following year to find cars.") @RequestParam("year") @Min(1) @Max(10) Integer year) {
 		return carService.findCarsAfterYear(year);
 	}
 	
