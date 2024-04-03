477486b2b1a1c3fa47602b10d3ce344baec47d09
logbasex <contact.hoducanh@gmail.com>
2024-04-03 16:15:22 +0700

    [Websocket] STOMP sub-protocol.

--- spring-websocket/src/main/java/com/logbasex/websocketdemo/config/WebsocketConfig.java
+++ spring-websocket/src/main/java/com/logbasex/websocketdemo/config/WebsocketConfig.java
@@ -9,7 +9,18 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
-	
+	/**
+	 * STOMP sub-protocol
+	 * WebSocket is the transport medium, STOMP is a standardized method of communication over a WebSocket. It's the same as HTTP is to TCP.
+	 * <p>
+	 * While WebSocket defines a protocol for bidirectional communication between client and server, it does not put any condition on the message to be exchanged.
+	 * This is left open for parties in the communication to agree as part of sub-protocol negotiation.
+	 * <p>
+	 * It’s not convenient to develop a sub-protocol for non-trivial applications. Fortunately, there are many
+	 * popular sub-protocols like STOMP available for use.
+	 * STOMP stands for Simple Text Oriented Messaging Protocol and works over WebSocket.
+	 * Spring Boot has first class support for STOMP, which we’ll make use of in our tutorial.
+	 */
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		//https://stackoverflow.com/questions/52731686/websocket-vs-sockjs-object
--- spring-websocket/src/main/java/com/logbasex/websocketdemo/controller/ChatController.java
+++ spring-websocket/src/main/java/com/logbasex/websocketdemo/controller/ChatController.java
@@ -9,7 +9,17 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class ChatController {
-	
+
+	/**
+	 * ^@MessageMapping("/chat.sendMessage"): This annotation indicates that the method sendMessage should be called
+	 * when a message is sent to the WebSocket destination /chat.sendMessage.
+	 * This is similar to how @RequestMapping works for HTTP requests, but it's for messages coming in from a WebSocket.
+	 * </p>
+	 * ^@SendTo("/topic/public"): This annotation specifies that the return value of the method should be sent
+	 * to the given destination, in this case, /topic/public.
+	 * This means that after sendMessage is called and processes the message, the ChatMessage object it returns
+	 * will be sent to all clients subscribed to the /topic/public broker channel.
+	 */
 	@MessageMapping("/chat.sendMessage")
 	@SendTo("/topic/public")
 	public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
