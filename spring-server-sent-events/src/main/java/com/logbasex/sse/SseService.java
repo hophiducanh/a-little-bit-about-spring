450be4d84774f72ffd40aa8f6805d9ac065882bb
logbasex <contact.hoducanh@gmail.com>
2024-04-29 10:42:58 +0700

    [SSE] WeakReference avoid memory leak.

--- spring-server-sent-events/src/main/java/com/logbasex/sse/SseService.java
+++ spring-server-sent-events/src/main/java/com/logbasex/sse/SseService.java
@@ -5,14 +5,20 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
+import java.lang.ref.WeakReference;
 import java.time.Instant;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 @Service
 public class SseService {
 
 	private final List<SseEmitter> emitters = new CopyOnWriteArrayList<>();
+
+	//When a client disconnects or the connection times out, the SseEmitter object becomes unreachable from the client's perspective.
+	// There are likely no other strong references holding onto it within the application logic
+	private ConcurrentHashMap<WeakReference<SseEmitter>, Integer> sseEmitters = new ConcurrentHashMap<>();
 
 	public void addEmitter(final SseEmitter emitter) {
 		emitters.add(emitter);
